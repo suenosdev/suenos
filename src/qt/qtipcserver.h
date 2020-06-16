0bb53f99833590b44218196def9034a820219c71
root <root@instance-5.us-central1-a.c.curious-chiller-266512.internal>
2020-06-16 12:12:31 +0000

    First commit

---
+++ src/qt/qtipcserver.h
@@ -0,0 +1,10 @@
+#ifndef QTIPCSERVER_H
+#define QTIPCSERVER_H
+
+// Define SUENOSCOIN-Qt message queue name
+#define BITCOINURI_QUEUE_NAME "SUENOSCOINURI"
+
+void ipcScanRelay(int argc, char *argv[]);
+void ipcInit(int argc, char *argv[]);
+
+#endif // QTIPCSERVER_H
